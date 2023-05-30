c997cda59d504a73e0b58efc535d203f8d1c7b53
Kirushka88 <kirushka8841@yandex.ru>
2023-05-30 20:46:35 +0600

    take mistakes

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ def format_contacts_list(contacts_list):
         # Записываем отформатированный контакт в новый список
         formatted_contact = parts + [organization] + [position] + [phone_number] + [email]
         formatted_contacts_list.append(formatted_contact)
+        old_phone_format = r"(8\s?|\+7\s?)\(?(\d+)\)?(\s|\-)?(\d+)(\s|\-)?(\d+)(\s|\-)?(\d+)"
+        new_phone_format = r"+7(\2)-\4-\6-\8"
+        old_add_phone_format = r"\(?(доб?)(\.?)(\s)(\d+)\)?"
+        new_add_phone_format = r"доб.\4"
+
+        for line in contacts_list:
+            line[5] = re.sub(old_phone_format, new_phone_format, line[5], flags=0)
+            line[5] = re.sub(old_add_phone_format, new_add_phone_format, line[5], flags=0)
     return formatted_contacts_list
 
 
@@ -41,16 +49,6 @@ def delete_copy(formatted_contacts_list):
             phone_dict[contact[0]] = contact
     return list(phone_dict.values())
 
-def return_number():
-    old_phone_format = r"(8\s?|\+7\s?)\(?(\d+)\)?(\s|\-)?(\d+)(\s|\-)?(\d+)(\s|\-)?(\d+)"
-    new_phone_format = r"+7(\2)-\4-\6-\8"
-    old_add_phone_format = r"\(?(доб?)(\.?)(\s)(\d+)\)?"
-    new_add_phone_format = r"доб.\4"
-
-    for line in contacts_list:
-        line[5] = re.sub(old_phone_format, new_phone_format, line[5], flags=0)
-        line[5] = re.sub(old_add_phone_format, new_add_phone_format, line[5], flags=0)
-
 
 formatted_contacts_list = format_contacts_list(contacts_list)
 new_formatted_contacts_list = delete_copy(formatted_contacts_list)
@@ -59,5 +57,4 @@ pprint(new_formatted_contacts_list)
 
 with open(r"phonebook_raw_new.csv", "w", encoding="utf-8") as f:
     datawriter = csv.writer(f, delimiter=',')
-    # Вместо contacts_list подставьте свой список
     datawriter.writerows(new_formatted_contacts_list)
